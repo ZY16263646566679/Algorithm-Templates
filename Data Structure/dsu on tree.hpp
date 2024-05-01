3f9130ee40856c2e8ce68609c37cb92171e67911
彭志勇 <2457305628@qq.com>
2024-05-01 21:32:39 +0800

    update：articulation point and bridge

--- Data Structure/dsu on tree.hpp
+++ Data Structure/dsu on tree.hpp
@@ -2,10 +2,12 @@
 #include <bits/stdc++.h>
 using namespace std;
 using ll = long long;
-const int N = 1e5 + 5;
+
 // 模板
+const int N = 1e5 + 5;
 int n, dep[N], sz[N], hson[N]; // 节点下标从1开始，hson[i]为0表示节点i没有重儿子
 vector<int> edges[N];
+
 void dfs0(int u, int fa = -1) {
     int mx = 0, size = 1;
     for (int v : edges[u])
@@ -16,24 +18,29 @@ void dfs0(int u, int fa = -1) {
         }
     sz[u] = size;
 }
+
 void add(int u) { // add operation
 
 }
+
 void del(int u) { // del operation
 
 }
+
 void addsubtree(int u, int fa = -1) { // 增加一个子树的统计信息
     add(u);
     for (int v : edges[u])
         if (v != fa)
             addsubtree(v, u);
 }
+
 void delsubtree(int u, int fa = -1) { // 删除一个子树的统计信息
     del(u);
     for (int v : edges[u])
         if (v != fa)
             delsubtree(v, u);
 }
+
 void dfs(int u, int fa = -1, bool keep = true) {
     for (int v : edges[u])
         if (v != fa && v != hson[u])
@@ -55,6 +62,7 @@ void dfs(int u, int fa = -1, bool keep = true) {
 // 模板题https://codeforces.com/problemset/problem/600/E
 int color[N], cnt[N], maxn; // 节点下标从1开始
 ll sum, ans[N]; // ans[i]：节点i的启发式合并结果
+
 void add(int u) { // 增加一个节点的统计信息
     int n = ++cnt[color[u]];
     if (n > maxn) {
@@ -63,6 +71,7 @@ void add(int u) { // 增加一个节点的统计信息
         sum += color[u];
     }
 }
+
 void del(int u) { // 删除一个节点的统计信息
     --cnt[color[u]];
 }
@@ -70,6 +79,7 @@ void del(int u) { // 删除一个节点的统计信息
 // 还可以利用子树的dfs序（DFS Numbering）连续的性质，直接循环而不是递归进行addsubtree和delsubtree
 // 修改后的两个dfs函数；不再需要单独写出addsubtree和delsubtree函数
 int sz[N], hson[N], L[N], R[N], node[N], cdfsn;
+
 void dfs0(int u, int fa = -1) {
     L[u] = ++cdfsn;
     node[cdfsn] = u;
@@ -83,6 +93,7 @@ void dfs0(int u, int fa = -1) {
     sz[u] = size;
     R[u] = cdfsn;
 }
+
 void dfs(int u, int fa = -1, bool keep = 1) {
     for (int v : edges[u])
         if (v != fa && v != hson[u])
--- Graph/Connectivity/Undirected Graph/articulation point and bridge.hpp
+++ Graph/Connectivity/Undirected Graph/articulation point and bridge.hpp
@@ -1,25 +1,48 @@
-// 求割点/割边（桥）
+// Tarjan 算法求割点/割边（桥）
 #include <bits/stdc++.h>
 using namespace std;
 
-const int N = 109;
+const int N = 2e4 + 5;
 int low[N], num[N], dfn; // root is node 1, num[u]: dfn of u
 bool iscut[N];
 vector<int> e[N]; // 邻接表存图
 
-void dfs(int u, int fa) {
+// 求割点
+void dfs(int u) {
     low[u] = num[u] = ++dfn;
     int child = 0;
-    for (int v : e[u]) { // u -> v
-        if (num[v]) continue; // v被访问过
+    for (int v : e[u]) { // u ←→ v
+        if (num[v]) { // v 被访问过
+            low[u] = min(low[u], num[v]);
+            continue;
+        }
         child++;
-        dfs(v, u);
-        if(low[v] < low[u]) low[u] = low[v]; // 用后代的low值更新low值
-        if (low[v] >= num[u] && u - 1) // 换成 > 就是求割边
+        dfs(v);
+        low[u] = min(low[u], low[v]); // 用后代的 low 值更新 low 值
+        if (low[v] >= num[u] && u - 1)
             iscut[u] = true;
     }
     if (u == 1 && child >= 2)
         iscut[1] = true;
 }
 
-// 在求割边时，如果iscnt[u]为真，(father[u], u)为一条割边
+// 求割边
+vector<pair<int, int>> bridge;
+
+void dfs(int u, int fa) {
+    low[u] = num[u] = ++dfn;
+    for (int v : e[u]) {
+        if (v == fa) continue;
+        if (num[v]) {
+            low[u] = min(low[u], num[v]);
+            continue;
+        }
+        dfs(v);
+        low[u] = min(low[u], low[v]);
+        if (low[v] > num[u]) // u ←→ v 是割边
+            bridge.push_back({u, v});
+    }
+    // 也可以这样写：
+    // if (low[u] == num[u] && fa != 0) // fa ←→ u 是割边
+    //     bridge.push_back({fa, u});
+}
