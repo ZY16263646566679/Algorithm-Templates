5a0ce41f219567becc5bf929158c4804e9c7c32c
彭志勇 <2457305628@qq.com>
2024-09-16 18:32:17 +0800

    update

--- Graph/Euler Path/Fleury.cpp
+++ Graph/Euler Path/Fleury.cpp
@@ -1,40 +1,36 @@
+// Fleury 算法求欧拉路径（一笔画），实现方法复杂，实用性不高
 #include <bits/stdc++.h>
 using namespace std;
 
 int ans[200], top, n, m, mp[200][200];
+
 void dfs(int x) {
-    int i;
     top++;
     ans[top] = x;
-    for (i = 1; i <= n; i++) {
-        if(mp[x][i] > 0) {
+    for (int i = 1; i <= n; i++)
+        if(mp[x][i]) {
             mp[x][i] = mp[i][x] = 0; // 删除此边
             dfs(i);
             break;
         }
-    }
 }
 
 void fleury(int x) {
     int brige, i;
     top = 1;
-    ans[top] = x; /// 将起点放入Euler路径中
+    ans[top] = x; // 将起点放入Euler路径中
     while (top >= 0) {
         brige = 0;
-        for (i = 1; i <= n; i++) /// 试图搜索一条边不是割边（桥）
-        {
-            if (mp[ans[top]][i] > 0) /// 存在一条可以扩展的边
-            {
+        for (i = 1; i <= n; i++) // 试图搜索一条边不是割边（桥）
+            if (mp[ans[top]][i]) { // 存在一条可以扩展的边
                 brige = 1;
                 break;
             }
-        }
         if (!brige) { // 如果没有点可以扩展，输出并出栈
             if (ans[top])
                 printf("%d ", ans[top]);
             top--;
-        }
-        else { // 否则继续搜索欧拉路径
+        } else { // 否则继续搜索欧拉路径
             top--; // 为了回溯
             dfs(ans[top + 1]);
         }
@@ -51,12 +47,11 @@ int main() {
         mp[y][x] = 1;
     }
     num = 0;
-    start = 1; /// 这里初始化为1
+    start = 1; // 这里初始化为1
     for (i = 1; i <= n; i++) {
         deg = 0;
-        for (j = 1; j <= n; j++) {
+        for (j = 1; j <= n; j++)
             deg += mp[i][j];
-        }
         if (deg % 2 == 1) {
             start = i;
             num++;
