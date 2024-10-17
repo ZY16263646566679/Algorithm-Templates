bbca6facdc12c0fec60af4d236c596f5c2b02aa4
彭志勇 <2457305628@qq.com>
2024-10-17 11:52:56 +0800

    update

--- Search/BFS with deque.cpp
+++ Search/BFS with deque.cpp
@@ -5,7 +5,7 @@ using pii = pair<int, int>;
 
 // 例题：https://www.luogu.com.cn/problem/P4667
 const int N = 505;
-const int dx[4] {1, 1, -1, -1}, dy[4] {1, -1, 1, -1};
+const int d[5] {1, 1, -1, -1, 1};
 int n, m, dis[N][N]; // dis[x][y] 表示从起点到 (x, y) 的最短距离
 bool g[N][N]; // g[i][j]: s[i][j] == '/'
 deque<pii> q;
@@ -19,11 +19,11 @@ int bfs() {
         if (x == n && y == m)
             return dis[n][m];
         for (int i = 0; i < 4; i++) {
-            int nx = x + dx[i], ny = y + dy[i];
-            int d = dx[i] != dy[i] ^ g[min(x, nx)][min(y, ny)];
-            if (0 <= nx && nx <= n && 0 <= ny && ny <= m && dis[x][y] + d < dis[nx][ny]) {
-                dis[nx][ny] = dis[x][y] + d;
-                d ? q.push_back({nx, ny}) : q.push_front({nx, ny});
+            int nx = x + d[i], ny = y + d[i + 1];
+            int s = d[i] != d[i + 1] ^ g[min(x, nx)][min(y, ny)];
+            if (0 <= nx && nx <= n && 0 <= ny && ny <= m && dis[x][y] + s < dis[nx][ny]) {
+                dis[nx][ny] = dis[x][y] + s;
+                s ? q.push_back({nx, ny}) : q.push_front({nx, ny});
             }
         }
     }
