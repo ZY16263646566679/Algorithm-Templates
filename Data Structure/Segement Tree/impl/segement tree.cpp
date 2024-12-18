9cc87a11aa3092084c38c07f6934bd0205af464e
彭志勇 <2457305628@qq.com>
2024-12-18 13:46:44 +0800

    update

--- Data Structure/Segement Tree/impl/segement tree.cpp
+++ Data Structure/Segement Tree/impl/segement tree.cpp
@@ -1,51 +1,64 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-#define mid (l + r >> 1)
-#define l(p) (p << 1)
-#define r(p) (p << 1 | 1)
-#define ls l(p), l, mid
-#define rs r(p), mid + 1, r
+#define l(p) (p * 2)
+#define r(p) (p * 2 + 1)
 
 const int N = 1e5 + 5;
-// 下标要从1开始，因为0 << 1并不是0号节点的左儿子
-int n, v[N + 1], t[N << 2], tag[N << 2];
+int n, v[N + 1];
 
-void up(int p) { t[p] = t[l(p)] + t[r(p)]; } // t[p] = max(t[ls(p)], t[rs(p)]);
+// 下标要从1开始，因为0 * 2并不是0号节点的左儿子
+struct node {
+    int l, r, v, tag;
+} t[N * 4];
 
-void build(int p, int l, int r) { // 区间[l, r]
-    tag[p] = 0;
-    if (l == r) { t[p] = v[l]; return; }
-    build(ls);
-    build(rs);
+void up(int p) {
+    // t[p] = max(t[ls(p)], t[rs(p)]);
+    t[p].v = t[l(p)].v + t[r(p)].v;
+}
+
+void build(int p, int l, int r) { // [l, r]
+    t[p].l = l, t[p].r = r, t[p].tag = 0;
+    if (l == r) {
+        t[p].v = v[l];
+        return;
+    }
+    int mid = l + r >> 1;
+    build(l(p), l, mid);
+    build(r(p), mid + 1, r);
     up(p);
 }
 
-void addtag(int p, int l, int r, int d) { // 如果只有单点修改，不需要用lazy-tag
-    t[p] += d * (r - l + 1); // 更新值
-    tag[p] += d; // 打标签
+void addtag(int p, int d) { // 如果只有单点修改，不需要用lazy-tag
+    t[p].v += d * (t[p].r - t[p].l + 1); // 更新值
+    t[p].tag += d; // 打标签
 }
 
-void down(int p, int l, int r) { // 标签往下传递，并更新子节点值
-    addtag(ls, tag[p]);
-    addtag(rs, tag[p]);
-    tag[p] = 0;
+void down(int p) { // 标签往下传递，并更新子节点值
+    if (!t[p].tag) return;
+    addtag(l(p), t[p].tag);
+    addtag(r(p), t[p].tag);
+    t[p].tag = 0;
 }
 
-void update(int L, int R, int p, int l, int r, int d) {
-    if (L <= l && r <= R) { addtag(p, l, r, d); return; }
-    if (tag[p]) down(p, l, r);
-    if (L <= mid) update(L, R, ls, d);
-    if (R > mid) update(L, R, rs, d);
+void update(int p, int L, int R, int d) { // [L, R]
+    if (L <= t[p].l && t[p].r <= R) {
+        addtag(p, d);
+        return;
+    }
+    down(p);
+    int mid = t[p].l + t[p].r >> 1;
+    if (L <= mid) update(l(p), L, R, d);
+    if (R > mid) update(r(p), L, R, d);
     up(p);
 }
 
-int query(int L, int R, int p, int l, int r) {
-    if (L <= l && r <= R) return t[p];
-    if (tag[p]) down(p, l, r);
+int query(int p, int L, int R) { // [L, R]
+    if (L <= t[p].l && t[p].r <= R) return t[p].v;
+    down(p);
+    int mid = t[p].l + t[p].r >> 1;
     int res = 0;
-    if (L <= mid) res += query(L, R, ls);
-    if (R > mid) res += query(L, R, rs);
-    // return max(query(L, mid, p << 1, l, mid), query(mid + 1, R, p << 1 | 1, mid + 1, R));
+    if (L <= mid) res += query(l(p), L, R);
+    if (R > mid) res += query(r(p), L, R);
     return res;
 }
